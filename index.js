b18243d91fb03749849e2f2fa6285e406f6a3862
David Walters <littledew13099@gmail.com>
2021-05-30 12:12:07 -0400

    update to api 0.0.14

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ bot.on.ready(async () => {
 });
 
 bot.commandHandler.register([
-  new WOLF.Command(`${keyword}_command_${keyword}`, { group: async (command) => await bot.messaging().sendGroupMessage(command.targetGroupId, bot.phrase().getByLanguageAndName(command.language, `${bot.config.keyword}_help_message`)) },
+  new WOLF.Command(`${keyword}_command_${keyword}`, { group:  (command) => bot.messaging().sendGroupMessage(command.targetGroupId, bot.phrase().getByLanguageAndName(command.language, `${bot.config.keyword}_help_message`)) },
     [
-      new WOLF.Command(`${keyword}_command_help`, { group: (command) => help(bot, command) }),
+      new WOLF.Command(`${keyword}_command_help`, { group: (command) => bot.messaging().sendGroupMessage(command.targetGroupId, bot.phrase().getByLanguageAndName(command.language, `${bot.config.keyword}_help_message`)) }),
       new WOLF.Command(`${keyword}_command_example`, { group: (command) => example(bot, command) }),
       new WOLF.Command(`${keyword}_command_capability`, { group: (command) => capabilityExample(bot, command) })
     ])
