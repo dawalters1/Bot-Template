f792b28ebb4b1750026252621cba692a5ce510a4
Dave <littledew13099@gmail.com>
2021-06-06 12:25:24 -0400

    Update index.js

--- src/example/index.js
+++ src/example/index.js
@@ -3,8 +3,8 @@ module.exports = async (api, command)=>{
     
     const subscriber = await api.subscriber().getById(command.sourceSubscriberId);
     
-    return await bot.messaging().sendGroupMessage(command.targetGroupId, 
-        bot.utility().string().replace(bot.phrase().getByLanguageAndName(command.language, `${api.config.keyword}_example_message`),
+    return await api.messaging().sendGroupMessage(command.targetGroupId, 
+        api.utility().string().replace(api.phrase().getByLanguageAndName(command.language, `${api.config.keyword}_example_message`),
         {
             nickname: subscriber.nickname,
             subscriberId: subscriber.id
