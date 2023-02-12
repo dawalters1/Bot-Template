f646f05df544f43da0a6212945f1ce730abb699f
Dave <littledew13099@gmail.com>
2023-02-11 19:16:49 -0500

    install rc package

--- index.js
+++ index.js
@@ -9,33 +9,38 @@ const client = new WOLF();
 
 const keyword = client.config.keyword;
 
-client.commandHandler.register([
-  // Base Command 1
-  new Command(`${keyword}_command_${keyword}`, { both: (command) => help(client, command) },
-    [
-      new Command(`${keyword}_command_help`, { both: (command) => help(client, command) }),
-      new Command(`${keyword}_command_get`, { group: (command) => help(client, command) },
-        [
-          new Command(`${keyword}_command_group`, { group: (command) => group(client, command) }),
-          new Command(`${keyword}_command_subscriber`, { group: (command) => subscriber(client, command) })
-        ]),
-      new Command(`${keyword}_command_join`, { both: (command) => join(client, command) }),
-      new Command(`${keyword}_command_leave`, { both: (command) => leave(client, command) })
-    ]
-  ),
+client.commandHandler.register(
+  [
+    // Base Command 1
+    new Command(`${keyword}_command_${keyword}`, { both: command => help(client, command) },
+      [
+        new Command(`${keyword}_command_help`, { both: command => help(client, command) }),
+        new Command(`${keyword}_command_get`, { group: command => help(client, command) },
+          [
+            new Command(`${keyword}_command_group`, { group: command => group(client, command) }),
+            new Command(`${keyword}_command_subscriber`, { group: command => subscriber(client, command) })
+          ]
+        ),
+        new Command(`${keyword}_command_join`, { both: command => join(client, command) }),
+        new Command(`${keyword}_command_leave`, { both: command => leave(client, command) })
+      ]
+    ),
 
-  // Base Command 2 - Example !blackjack
-  new Command(`${keyword}_command_example2`, { both: (command) => help(client, command, 'example2') },
-    [
-    // Add blackjack commands here
-    ]),
+    // Base Command 2 - Example !blackjack
+    new Command(`${keyword}_command_example2`, { both: command => help(client, command, 'example2') },
+      [
+        // Add blackjack commands here
+      ]
+    ),
 
-  // Base Command 3 - Example !roulette
-  new Command(`${keyword}_command_example3`, { both: (command) => help(client, command, 'example3') },
-    [
-    // Add roulette commands here
-    ])
-]);
+    // Base Command 3 - Example !roulette
+    new Command(`${keyword}_command_example3`, { both: command => help(client, command, 'example3') },
+      [
+        // Add roulette commands here
+      ]
+    )
+  ]
+);
 
 client.on('ready', () => console.log('Ready'));
 
